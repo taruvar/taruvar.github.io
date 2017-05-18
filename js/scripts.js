1a75406cd547260aec8a14a2e3410b8918e52611
Taruvar Aggarwal <taruvar@gmail.com>
2017-05-19 01:49:06 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -14,38 +14,6 @@
 	    });
 	}());
 
-
-
-    // ---------------------------------------------- 
-    //  magnific-popup
-    // ----------------------------------------------
-	(function () {
-
-		$('.portfolio-items').magnificPopup({ 
-			delegate: 'a',
-			type: 'image',
-			// other options
-			closeOnContentClick: false,
-			closeBtnInside: false,
-			mainClass: 'mfp-with-zoom mfp-img-mobile',
-
-			gallery: {
-				enabled: false
-			},
-			zoom: {
-				enabled: true,
-				duration: 300, // don't foget to change the duration also in CSS
-				opener: function(element) {
-					return element.find('i');
-				}
-			}
-
-		});
-
-	}()); 
-
-
-
     // ---------------------------------------------- 
     // Fun facts
     // ---------------------------------------------- 
